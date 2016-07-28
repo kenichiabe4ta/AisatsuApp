4c492ff36a00684f111191a695dd2ace2d916cd5
kenichiabe4ta <sute54webedu@yahoo.co.jp>
2016-07-29 01:10:48 +0900

    最初のコミット

---
+++ app/src/main/java/jp/techacademy/kenichi/abe/aisatsuapp/MainActivity.java
@@ -0,0 +1,60 @@
+package jp.techacademy.kenichi.abe.aisatsuapp;
+import android.app.TimePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import java.util.Calendar;
+public class MainActivity extends AppCompatActivity {
+
+    TextView mTextView;
+    String Message="";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTextView = (TextView) findViewById(R.id.textView);
+    }
+
+    public void showTimePickerDialog(View v){
+        Calendar calendar = Calendar.getInstance();
+        int chour = calendar.get(Calendar.HOUR_OF_DAY);
+        int cminute = calendar.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        Message = SetMassage(hourOfDay);
+                        Log.d("test","設定時刻="+hourOfDay+":"+minute);
+                    }
+                },
+                chour, // 初期値（時間）
+                cminute,  // 初期値（分）
+                true);
+        timePickerDialog.show();
+    }
+
+    private String SetMassage(int h){
+        String Message="";
+        if( (h>=2)&&(h<=9) ){
+            Message="おはよう";
+        }else if( (h>=10)&&(h<=17) ){
+            Message="こんにちは";
+        }else if( (h>=18)&&(h<=23) || (h>=0)&&(h<=1) ){
+            Message="こんばんは";
+        }
+        return Message;
+    }
+
+    public void DisplayTime(View v){
+        if(Message != "" ){
+            mTextView.setText(Message);
+            Log.d("test",Message);
+        }
+    }
+
+}
